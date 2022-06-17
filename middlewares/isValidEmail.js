0fd5c206072022bc34988a78c4b25a5f89740121
fumagallilaura <la_12_msn@hotmail.com>
2022-06-16 23:56:49 -0300

    fix: email validation

--- middlewares/isValidEmail.js
+++ middlewares/isValidEmail.js
@@ -3,7 +3,7 @@ const { invalidEmail } = require('../schemas/loginValidations.js');
 const isEmailValid = (req, res, next) => {
   const { email } = req.body;
   
-  if (!email.match(/.+@.+\..+/)) return res.status(400).json(invalidEmail);
+  if (!email.match(/.+@.+\.com?.+/)) return res.status(400).json(invalidEmail.error);
 
   next();
 };
