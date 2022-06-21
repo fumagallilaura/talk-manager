f6fa6c7854fdbdd9a85176e5fcbf02ebcc45b88f
fumagallilaura <la_12_msn@hotmail.com>
2022-06-21 17:26:32 -0300

    fix: update json return

--- controllers/updateTalker.js
+++ controllers/updateTalker.js
@@ -3,23 +3,22 @@ const { OK } = require('../schemas/httpCodes');
 const getTalkerById = require('../service/getTalkerById');
 const createTalkers = require('../repository/createTalkers');
 const isEmpty = require('../service/isEmpty');
-const updateTalkerService = require('../service/updateTalker');
 
 const updateTalker = async (req, res) => {
   const { body: { name, age, talk }, params: { id } } = req;
-  
+
   const talker = await getTalkerById(id);
-  if (isEmpty(talker)) return res.status(404).json({ message: 'Palestrante não encontrado' });
-
   const talkers = await getAllTalkers();
+  const talkerIndex = talkers.findIndex(({ id: findId }) => findId === talker.id);
 
-  const talkerIndex = talkers.indexOf(talker);
-  console.log('talker index ', talkerIndex);
+  if (isEmpty(talker) || talkerIndex === -1) {
+    return res.status(404).json({ message: 'Palestrante não encontrado' });
+  }
+
   const updatedTalker = { ...talkers[talkerIndex], name, age, talk };
   talkers[talkerIndex] = updatedTalker;
-  console.log('talkers ', talkers);
   await createTalkers(talkers);
-  return res.status(OK).json([updatedTalker]);
+  return res.status(OK).json(updatedTalker);
 };
 
 module.exports = updateTalker;
