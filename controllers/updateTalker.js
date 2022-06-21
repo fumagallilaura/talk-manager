8d2ed464ec9528aeab37bcfebe223c21b0430129
fumagallilaura <la_12_msn@hotmail.com>
2022-06-21 16:39:39 -0300

    fix: import in update controller

--- controllers/updateTalker.js
+++ controllers/updateTalker.js
@@ -2,18 +2,22 @@ const getAllTalkers = require('../repository/getAllTalkers');
 const { OK } = require('../schemas/httpCodes');
 const getTalkerById = require('../service/getTalkerById');
 const createTalkers = require('../repository/createTalkers');
+const isEmpty = require('../service/isEmpty');
+const updateTalkerService = require('../service/updateTalker');
 
 const updateTalker = async (req, res) => {
   const { body: { name, age, talk }, params: { id } } = req;
-
+  
   const talker = await getTalkerById(id);
-  if (!talker) return res.status(404).json({ message: 'Palestrante não encontrado' });
+  if (isEmpty(talker)) return res.status(404).json({ message: 'Palestrante não encontrado' });
+
   const talkers = await getAllTalkers();
 
   const talkerIndex = talkers.indexOf(talker);
+  console.log('talker index ', talkerIndex);
   const updatedTalker = { ...talkers[talkerIndex], name, age, talk };
   talkers[talkerIndex] = updatedTalker;
-
+  console.log('talkers ', talkers);
   await createTalkers(talkers);
   return res.status(OK).json([updatedTalker]);
 };
